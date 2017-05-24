eb23dbbf7527447880dd7014dc161c027b6c8f66
Chris Raynor <raynor@google.com>
2017-05-24 14:04:30 -0700

    Removing all but the current API. Will re-add auth later

--- internal/internal.go
+++ internal/internal.go
@@ -4,29 +4,17 @@ package internal
 import (
 	"net/http"
 	"net/url"
-
-	"github.com/firebase/firebase-admin-go/credentials"
 )
-
-// AppService represents a service initialized and managed by a Firebase App.
-//
-// Each Firebase service exposed from the Admin SDK should implement this interface. This enables the parent Firebase
-// App to gracefully terminate Firebase services when they are no longer needed.
-type AppService interface {
-	// Del gracefully terminates this AppService by cleaning up any internal state, and releasing any resources
-	// allocated.
-	Del()
-}
-
-// AppConf represents the internal state of a Firebase App that is shared across all Firebase services.
-type AppConf struct {
-	Name string
-	Cred credentials.Credential
-}
 
 // DatabaseConfig contains all the configuration needed to initialize a database
 // client.
 type DatabaseConfig struct {
 	Client *http.Client
 	URL    *url.URL
+}
+
+// AuthConfig contains all the configuration needed to initialize an auth
+// client.
+type AuthConfig struct {
+	Client *http.Client
 }
