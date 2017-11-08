41fe3aa4c0cbee6d06c3d68628d84c373fab55a2
Avishalom Shalit <avishalom@gmail.com>
2017-11-08 17:14:10 -0500

    server

--- auth/user_mgt_test.go
+++ auth/user_mgt_test.go
@@ -2,41 +2,82 @@ package auth
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
+
+	"firebase.google.com/go/internal"
+	"golang.org/x/net/context"
+	"google.golang.org/api/option"
 )
 
-type mockServer struct {
+type testReq struct {
+	Method string
+	Path   string
+	Header http.Header
+	Body   []byte
+	Query  map[string]string
+}
+
+func newTestReq(r *http.Request) (*testReq, error) {
+	defer r.Body.Close()
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	u, err := url.Parse(r.RequestURI)
+	if err != nil {
+		return nil, err
+	}
+
+	query := make(map[string]string)
+	for k, v := range u.Query() {
+		query[k] = v[0]
+	}
+	return &testReq{
+		Method: r.Method,
+		Path:   u.Path,
+		Header: r.Header,
+		Body:   b,
+		Query:  query,
+	}, nil
+}
+
+type mockAuthServer struct {
 	Resp   interface{}
 	Header map[string]string
 	Status int
+	Reqs   []*testReq
 	srv    *httptest.Server
 }
 
-func (s *mockServer) Start(c *Client) *httptest.Server {
-	if s.srv != nil {
-		return s.srv
-	}
+func EchoServer(resp interface{}) *mockAuthServer {
+	s := mockAuthServer{Resp: resp}
+
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tr, _ := newTestReq(r)
+		s.Reqs = append(s.Reqs, tr)
+
 		for k, v := range s.Header {
 			w.Header().Set(k, v)
 		}
-
-		print := r.URL.Query().Get("print")
 		if s.Status != 0 {
 			w.WriteHeader(s.Status)
-		} else if print == "silent" {
-			w.WriteHeader(http.StatusNoContent)
-			return
 		}
 		b, _ := json.Marshal(s.Resp)
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(b)
 	})
 	s.srv = httptest.NewServer(handler)
-	//c.url = s.srv.URL
-	return s.srv
+	authClient, err := NewClient(context.Background(),
+		&internal.AuthConfig{
+			Opts: []option.ClientOption{option.WithHTTPClient(s.srv.Client())},
+		})
+	client.transportClient.Client = s.srv.Client()
+	return &s
 }
 
 func TestExportPayload(t *testing.T) {
@@ -45,5 +86,7 @@ func TestExportPayload(t *testing.T) {
 }
 
 func TestGetUser(t *testing.T) {
+	srv := EchoServer(client, "message5")
 
+	t.Errorf("%v", client)
 }
