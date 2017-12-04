4963de4e1d78326bcbab1d041897c81ba7130799
Avishalom Shalit <avishalom@gmail.com>
2017-12-04 13:31:22 -0500

    Removethe +1 second for the token timestamp.

--- auth/auth.go
+++ auth/auth.go
@@ -225,7 +225,7 @@ func (c *Client) VerifyIDToken(idToken string) (*Token, error) {
 	} else if p.Issuer != issuer {
 		err = fmt.Errorf("ID token has invalid 'iss' (issuer) claim. Expected %q but got %q. %s %s",
 			issuer, p.Issuer, projectIDMsg, verifyTokenMsg)
-	} else if p.IssuedAt > clk.Now().Unix()+1 { // adding 1 second, due to flakiness in sync.
+	} else if p.IssuedAt > clk.Now().Unix() {
 		err = fmt.Errorf("ID token issued at future timestamp: %d", p.IssuedAt)
 	} else if p.Expires < clk.Now().Unix() {
 		err = fmt.Errorf("ID token has expired. Expired at: %d", p.Expires)
