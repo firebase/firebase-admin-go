df168174ccaa286c74b7fc6d8e2ece3d674f865e
Avishalom Shalit <avishalom@gmail.com>
2017-12-06 08:11:46 -0500

    addressing comments opt branch/ BEFORE hc change

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -46,7 +46,6 @@ func TestMain(m *testing.M) {
 		ctx   context.Context
 		creds *google.DefaultCredentials
 	)
-	opt := option.WithCredentialsFile("../testdata/service_account.json")
 	if appengine.IsDevAppServer() {
 		aectx, aedone, err := aetest.NewContext()
 		if err != nil {
@@ -61,6 +60,7 @@ func TestMain(m *testing.M) {
 		}
 	} else {
 		ctx = context.Background()
+		opt := option.WithCredentialsFile("../testdata/service_account.json")
 		creds, err = transport.Creds(ctx, opt)
 		if err != nil {
 			log.Fatalln(err)
