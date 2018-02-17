f3f8c522bc18d457d117468298bf57d951037363
Avishalom Shalit <avishalom@gmail.com>
2018-02-16 21:57:40 -0500

    auth.go fix

--- snippets/auth.go
+++ snippets/auth.go
@@ -23,6 +23,7 @@ import (
 
 	"google.golang.org/api/iterator"
 )
+
 // Copyright 2017 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -36,16 +37,6 @@ import (
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package snippets
-
-import (
-	"context"
-	"log"
-
-	firebase "firebase.google.com/go"
-	"firebase.google.com/go/auth"
-)
 
 // ==================================================================
 // https://firebase.google.com/docs/auth/admin/create-custom-tokens
