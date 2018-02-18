fd78f9dd24950ed9333ebf2299128c3faedb2e15
Cyrille Hemidy <cyrille.hemidy@gmail.com>
2018-02-18 03:36:37 +0100

    Lint (#96)
    * fix misspelling
    
    * add check error
    
    * missing copyright

--- firebase_test.go
+++ firebase_test.go
@@ -574,7 +574,7 @@ func overwriteEnv(varName, newVal string) string {
 	return oldVal
 }
 
-// reinstateEnv restores the enviornment variable, will usually be used deferred with overwriteEnv.
+// reinstateEnv restores the environment variable, will usually be used deferred with overwriteEnv.
 func reinstateEnv(varName, oldVal string) {
 	if len(varName) > 0 {
 		os.Setenv(varName, oldVal)
