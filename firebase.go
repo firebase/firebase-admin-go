2fef7503e0d70f3b8810b8df18a44a5dda33c870
hiranya911 <hiranya911@gmail.com>
2018-09-14 10:11:33 -0700

    Bumped version to 3.4.0

--- firebase.go
+++ firebase.go
@@ -42,7 +42,7 @@ import (
 var defaultAuthOverrides = make(map[string]interface{})
 
 // Version of the Firebase Go Admin SDK.
-const Version = "3.3.0"
+const Version = "3.4.0"
 
 // firebaseEnvName is the name of the environment variable with the Config.
 const firebaseEnvName = "FIREBASE_CONFIG"
