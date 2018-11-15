c234000c523f24d9019adf3dce83503943575fef
hiranya911 <hiranya911@gmail.com>
2018-11-14 17:15:11 -0800

    Fixed formatting for go 1.11

--- messaging/messaging_test.go
+++ messaging/messaging_test.go
@@ -1028,7 +1028,7 @@ func checkIIDRequest(t *testing.T, b []byte, tr *http.Request, op string) {
 		t.Fatal(err)
 	}
 	want := map[string]interface{}{
-		"to": "/topics/test-topic",
+		"to":                  "/topics/test-topic",
 		"registration_tokens": []interface{}{"id1", "id2"},
 	}
 	if !reflect.DeepEqual(parsed, want) {
