71ed0be8e8c6688a617540fcdb95e9cff91bd5d6
Hiranya Jayathilaka <hiranya911@gmail.com>
2019-12-05 09:51:13 -0800

    More integration tests for tenant management support (#306)
    * Added more integration tests
    
    * Added SAML and OIDC integration tests
    
    * Added ImportUsers integration test
    
    * Updated ID token verification test

--- integration/auth/tenant_mgt_test.go
+++ integration/auth/tenant_mgt_test.go
@@ -17,8 +17,10 @@ package auth
 import (
 	"context"
 	"log"
+	"net/url"
 	"reflect"
 	"testing"
+	"time"
 
 	"firebase.google.com/go/auth"
 	"google.golang.org/api/iterator"
@@ -95,6 +97,28 @@ func TestTenantManager(t *testing.T) {
 		}
 	})
 
+	t.Run("UserManagement", func(t *testing.T) {
+		testTenantAwareUserManagement(t, id)
+	})
+
+	t.Run("OIDCProviderConfig", func(t *testing.T) {
+		tenantClient, err := client.TenantManager.AuthForTenant(id)
+		if err != nil {
+			t.Fatalf("AuthForTenant() = %v", err)
+		}
+
+		testOIDCProviderConfig(t, tenantClient)
+	})
+
+	t.Run("SAMLProviderConfig", func(t *testing.T) {
+		tenantClient, err := client.TenantManager.AuthForTenant(id)
+		if err != nil {
+			t.Fatalf("AuthForTenant() = %v", err)
+		}
+
+		testSAMLProviderConfig(t, tenantClient)
+	})
+
 	t.Run("UpdateTenant()", func(t *testing.T) {
 		want = &auth.Tenant{
 			ID:                    id,
@@ -114,10 +138,6 @@ func TestTenantManager(t *testing.T) {
 		if !reflect.DeepEqual(tenant, want) {
 			t.Errorf("UpdateTenant() = %#v; want = %#v", tenant, want)
 		}
-	})
-
-	t.Run("UserManagement", func(t *testing.T) {
-		testTenantAwareUserManagement(t, id)
 	})
 
 	t.Run("DeleteTenant()", func(t *testing.T) {
@@ -190,6 +210,157 @@ func testTenantAwareUserManagement(t *testing.T, id string) {
 		}
 	})
 
+	t.Run("Users()", func(t *testing.T) {
+		iter := tenantClient.Users(context.Background(), "")
+		var target *auth.ExportedUserRecord
+		for {
+			got, err := iter.Next()
+			if err == iterator.Done {
+				break
+			} else if err != nil {
+				t.Fatalf("Users() = %v", err)
+			}
+
+			if got.UID == user.UID {
+				target = got
+				break
+			}
+		}
+
+		if target == nil {
+			t.Fatalf("Users() did not return required user: %q", user.UID)
+		}
+
+		if !reflect.DeepEqual(*target.UserInfo, want) {
+			t.Errorf("Users() = %v; want = %v", *target.UserInfo, want)
+		}
+	})
+
+	t.Run("SetCustomUserClaims()", func(t *testing.T) {
+		claims := map[string]interface{}{
+			"premium": true,
+			"role":    "customer",
+		}
+		if err := tenantClient.SetCustomUserClaims(context.Background(), user.UID, claims); err != nil {
+			t.Fatalf("SetCustomUserClaims() = %v", err)
+		}
+
+		got, err := tenantClient.GetUser(context.Background(), user.UID)
+		if err != nil {
+			t.Fatalf("GetUser() = %v", err)
+		}
+
+		if !reflect.DeepEqual(got.CustomClaims, claims) {
+			t.Errorf("CustomClaims = %v; want = %v", got.CustomClaims, claims)
+		}
+	})
+
+	t.Run("EmailVerificationLink()", func(t *testing.T) {
+		link, err := tenantClient.EmailVerificationLink(context.Background(), want.Email)
+		if err != nil {
+			t.Fatalf("EmailVerificationLink() = %v", err)
+		}
+
+		tenant, err := extractTenantID(link)
+		if err != nil {
+			t.Fatalf("EmailVerificationLink() = %v", err)
+		}
+
+		if id != tenant {
+			t.Fatalf("EmailVerificationLink() TenantID = %q; want = %q", tenant, id)
+		}
+	})
+
+	t.Run("PasswordResetLink()", func(t *testing.T) {
+		link, err := tenantClient.PasswordResetLink(context.Background(), want.Email)
+		if err != nil {
+			t.Fatalf("PasswordResetLink() = %v", err)
+		}
+
+		tenant, err := extractTenantID(link)
+		if err != nil {
+			t.Fatalf("PasswordResetLink() = %v", err)
+		}
+
+		if id != tenant {
+			t.Fatalf("PasswordResetLink() TenantID = %q; want = %q", tenant, id)
+		}
+	})
+
+	t.Run("EmailSignInLink()", func(t *testing.T) {
+		link, err := tenantClient.EmailSignInLink(context.Background(), want.Email, &auth.ActionCodeSettings{
+			URL:             continueURL,
+			HandleCodeInApp: false,
+		})
+		if err != nil {
+			t.Fatalf("EmailSignInLink() = %v", err)
+		}
+
+		tenant, err := extractTenantID(link)
+		if err != nil {
+			t.Fatalf("EmailSignInLink() = %v", err)
+		}
+
+		if id != tenant {
+			t.Fatalf("EmailSignInLink() TenantID = %q; want = %q", tenant, id)
+		}
+	})
+
+	t.Run("RevokeRefreshTokens()", func(t *testing.T) {
+		validSinceMillis := time.Now().Unix() * 1000
+		time.Sleep(1 * time.Second)
+		if err := tenantClient.RevokeRefreshTokens(context.Background(), user.UID); err != nil {
+			t.Fatalf("RevokeRefreshTokens() = %v", err)
+		}
+
+		got, err := tenantClient.GetUser(context.Background(), user.UID)
+		if err != nil {
+			t.Fatalf("GetUser() = %v", err)
+		}
+
+		if got.TokensValidAfterMillis < validSinceMillis {
+			t.Fatalf("RevokeRefreshTokens() TokensValidAfterMillis (%d) < Now (%d)", got.TokensValidAfterMillis, validSinceMillis)
+		}
+	})
+
+	t.Run("ImportUsers()", func(t *testing.T) {
+		scrypt, passwordHash, err := newScryptHash()
+		if err != nil {
+			t.Fatalf("newScryptHash() = %v", err)
+		}
+
+		uid := randomUID()
+		email := randomEmail(uid)
+		user := (&auth.UserToImport{}).
+			UID(uid).
+			Email(email).
+			PasswordHash(passwordHash).
+			PasswordSalt([]byte("NaCl"))
+		result, err := tenantClient.ImportUsers(context.Background(), []*auth.UserToImport{user}, auth.WithHash(scrypt))
+		if err != nil {
+			t.Fatalf("ImportUsers() = %v", err)
+		}
+
+		defer func() {
+			tenantClient.DeleteUser(context.Background(), uid)
+		}()
+
+		if result.SuccessCount != 1 || result.FailureCount != 0 {
+			t.Errorf("ImportUsers() = %#v; want = {SuccessCount: 1, FailureCount: 0}", result)
+		}
+
+		savedUser, err := tenantClient.GetUser(context.Background(), uid)
+		if err != nil {
+			t.Fatalf("GetUser() = %v", err)
+		}
+		if savedUser.Email != email {
+			t.Errorf("ImportUser() Email = %q; want = %q", savedUser.Email, email)
+		}
+		if savedUser.TenantID != id {
+			t.Errorf("ImportUser() TenantID = %q; want = %q", savedUser.TenantID, id)
+		}
+	})
+
 	t.Run("DeleteUser()", func(t *testing.T) {
 		if err := tenantClient.DeleteUser(context.Background(), user.UID); err != nil {
 			t.Fatalf("DeleteUser() = %v", err)
@@ -199,6 +370,15 @@ func testTenantAwareUserManagement(t *testing.T, id string) {
 		if err == nil || !auth.IsUserNotFound(err) {
 			t.Errorf("Tenant() = %v; want = UserNotFound", err)
 		}
-
 	})
+}
+
+func extractTenantID(actionLink string) (string, error) {
+	u, err := url.Parse(actionLink)
+	if err != nil {
+		return "", err
+	}
+
+	q := u.Query()
+	return q.Get("tenantId"), nil
 }
