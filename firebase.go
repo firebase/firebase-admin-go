f26af19b4df388bccfa7f979e31a72276d75e636
Hiranya Jayathilaka <hiranya911@gmail.com>
2020-05-21 11:20:23 -0700

    [chore] Release 3.13.0 (#374)

--- firebase.go
+++ firebase.go
@@ -38,7 +38,7 @@ import (
 var defaultAuthOverrides = make(map[string]interface{})
 
 // Version of the Firebase Go Admin SDK.
-const Version = "3.12.1"
+const Version = "3.13.0"
 
 // firebaseEnvName is the name of the environment variable with the Config.
 const firebaseEnvName = "FIREBASE_CONFIG"
