affd79bd7f73599fc0d7b43aa6204e4844f20479
chong-shao <31256040+chong-shao@users.noreply.github.com>
2021-08-09 08:02:33 -0700

    Update ref_test.go

--- db/ref_test.go
+++ db/ref_test.go
@@ -333,7 +333,7 @@ func TestUnexpectedHttpError(t *testing.T) {
 		})
 	}
 
-	wantReqs := len(testOps) * (1 + defaultMaxRetries)
+	wantReqs := len(testOps)
 	if len(mock.Reqs) != wantReqs {
 		t.Errorf("Requests = %d; want = %d", len(mock.Reqs), wantReqs)
 	}
