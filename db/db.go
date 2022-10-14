fda4e909b7aa3a07067020d67a175e2995614c6d
Ryan LaForge <rajlaforge@gmail.com>
2022-10-14 13:19:17 -0600

    [rtdb emulator]: add the emulator url as an option

--- db/db.go
+++ db/db.go
@@ -18,9 +18,9 @@ package db
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/url"
-	"os"
 	"runtime"
 	"strings"
 
@@ -32,13 +32,16 @@ const userAgentFormat = "Firebase/HTTP/%s/%s/AdminGo"
 const invalidChars = "[].#$"
 const authVarOverride = "auth_variable_override"
 const emulatorHostEnvVar = "FIREBASE_DATABASE_EMULATOR_HOST"
+const emulatorNamespaceParam = "ns"
+
+var ErrInvalidURL error = errors.New("invalid database url")
 
 /** TODO:
  * Using https://github.com/firebase/firebase-admin-python/pull/313/files as the example
  * Validate // is not in the url
  * Include ?ns={namespace} query parameter
  * Use emulator admin credentials
- * Fill in parseDatabaseURL
+ * Fill in parseURLConfig
  * Make a set of credentials that correspond to the emulator
  * Additional tests for parsing the url
  * Ensure the parameters are passed on the url
@@ -47,8 +50,19 @@ const emulatorHostEnvVar = "FIREBASE_DATABASE_EMULATOR_HOST"
 // Client is the interface for the Firebase Realtime Database service.
 type Client struct {
 	hc           *internal.HTTPClient
-	url          string
+	dbURLConfig  *dbURLConfig
 	authOverride string
+}
+
+type dbURLConfig struct {
+	// BaseURL can be either:
+	//	- a production url (https://foo-bar.firebaseio.com/)
+	//	- an emulator url (http://localhost:8080)
+	BaseURL string
+
+	// Namespace is used in for the emulator to specify the databaseName
+	// To specify a namespace on your url, pass ns=<database_name> (localhost:8080/?ns=foo-bar)
+	Namespace string
 }
 
 // NewClient creates a new instance of the Firebase Database Client.
@@ -56,22 +70,12 @@ type Client struct {
 // This function can only be invoked from within the SDK. Client applications should access the
 // Database service through firebase.App.
 func NewClient(ctx context.Context, c *internal.DatabaseConfig) (*Client, error) {
-	var databaseURL string
-	emulatorHost := os.Getenv(emulatorHostEnvVar)
-	if emulatorHost == "" {
-		p, err := url.ParseRequestURI(c.URL)
-		if err != nil {
-			return nil, err
-		} else if p.Scheme != "https" {
-			return nil, fmt.Errorf("invalid database URL: %q; want scheme: %q", c.URL, "https")
-		}
-		databaseURL = fmt.Sprintf("https://%s", p.Host)
-	} else {
-		databaseURL = emulatorHost
+	urlConfig, err := parseURLConfig(c.URL)
+	if err != nil {
+		return nil, err
 	}
 
 	var ao []byte
-	var err error
 	if c.AuthOverride == nil || len(c.AuthOverride) > 0 {
 		ao, err = json.Marshal(c.AuthOverride)
 		if err != nil {
@@ -90,7 +94,7 @@ func NewClient(ctx context.Context, c *internal.DatabaseConfig) (*Client, error)
 	hc.CreateErrFn = handleRTDBError
 	return &Client{
 		hc:           hc,
-		url:          databaseURL,
+		dbURLConfig:  urlConfig,
 		authOverride: string(ao),
 	}, nil
 }
@@ -117,9 +121,12 @@ func (c *Client) sendAndUnmarshal(
 		return nil, fmt.Errorf("invalid path with illegal characters: %q", req.URL)
 	}
 
-	req.URL = fmt.Sprintf("%s%s.json", c.url, req.URL)
+	req.URL = fmt.Sprintf("%s%s.json", c.dbURLConfig.BaseURL, req.URL)
 	if c.authOverride != "" {
 		req.Opts = append(req.Opts, internal.WithQueryParam(authVarOverride, c.authOverride))
+	}
+	if c.dbURLConfig.Namespace != "" {
+		req.Opts = append(req.Opts, internal.WithQueryParam(emulatorNamespaceParam, c.dbURLConfig.Namespace))
 	}
 
 	return c.hc.DoAndUnmarshal(ctx, req, v)
@@ -148,15 +155,56 @@ func handleRTDBError(resp *internal.Response) error {
 	return err
 }
 
-// parseDatabaseURL returns the baseURL for the database
-// The input can be either be:
-// - a production URL (https://foo-bar.firebaseio.com/)
-// - an Emulator URL (http://localhost:8080/?ns=foo-bar)
-// In case of Emulator URL, the caller should ensure the namespace is extracted from the query param ns.
-// The resulting base_url never includes query params.
-// If url is a production URL and emulator_host is specified, the resulting base URL will use the emulator_host
-//
-//	emulator_host is ignored if url is already an emulator URL.
-func parseDatabaseURL() string {
-	return ""
+// parseURLConfig returns the dbURLConfig for the database
+// dbURL may be a production url (https://foo-bar.firebaseio.com/) or an emulator URL (localhost:8080/?ns=foo-bar)
+// The following rules will apply for determining the output:
+//   - If the url has no scheme it will be assumed to be an emulator url and be used.
+//   - else If the FIREBASE_DATABASE_EMULATOR_HOST environment variable is set it will be used.
+//   - else the url will be assumed to be a production url and be used.
+func parseURLConfig(dbURL string) (*dbURLConfig, error) {
+	parsedURL, err := url.ParseRequestURI(dbURL)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", dbURL, ErrInvalidURL)
+	}
+	if parsedURL.Scheme != "https" {
+		return parseEmulatorHost(dbURL, parsedURL)
+	}
+	return &dbURLConfig{
+		BaseURL:   dbURL,
+		Namespace: "",
+	}, nil
 }
+
+func parseEmulatorHost(rawEmulatorHostURL string, parsedEmulatorHost *url.URL) (*dbURLConfig, error) {
+	if strings.Contains(rawEmulatorHostURL, "//") {
+		return nil, fmt.Errorf(`invalid %s: "%s". It must follow format "host:port": %w`, emulatorHostEnvVar, rawEmulatorHostURL, ErrInvalidURL)
+	}
+
+	baseURL := parsedEmulatorHost.Host
+	if parsedEmulatorHost.Scheme != "http" {
+		baseURL = "http://" + baseURL
+	}
+
+	namespace := parsedEmulatorHost.Query().Get(emulatorNamespaceParam)
+	if namespace == "" {
+		return nil, fmt.Errorf(`invalid database URL: "%s". Database URL must be a valid URL to a Firebase Realtime Database instance (include ?ns=<db-name> query param)`, parsedEmulatorHost)
+	}
+
+	return &dbURLConfig{
+		BaseURL:   baseURL,
+		Namespace: namespace,
+	}, nil
+}
+
+/**
+"""Parses emulator URL like http://localhost:8080/?ns=foo-bar"""
+        query_ns = parse.parse_qs(parsed_url.query).get('ns')
+        if parsed_url.scheme != 'http' or (not query_ns or len(query_ns) != 1 or not query_ns[0]):
+            raise ValueError(
+                'Invalid database URL: "{0}". Database URL must be a valid URL to a '
+                'Firebase Realtime Database instance.'.format(parsed_url.geturl()))
+
+        namespace = query_ns[0]
+        base_url = '{0}://{1}'.format(parsed_url.scheme, parsed_url.netloc)
+        return base_url, namespace
+*/
