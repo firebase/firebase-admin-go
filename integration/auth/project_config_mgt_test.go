dfd8a36eef49713d2cc6e2bf691e8ceb95c11c20
Pragati <pragatimodi@google.com>
2023-03-15 18:38:16 -0700

    feat(auth): Add TOTP support in Project and Tenant config 1. Added multi_factor_config_mgt 2. Added project_config_mgt 3. Added MultiFactorConfig to Tenant struct 4. Added validation to Tenant request 5. Integration tests for ProjectConfig and Tenant

---
+++ integration/auth/project_config_mgt_test.go
@@ -0,0 +1,65 @@
+// Copyright 2023 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package auth
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"testing"
+
+	"firebase.google.com/go/v4/auth"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestProjectConfig(t *testing.T) {
+	mfaObject := &auth.MultiFactorConfig{
+		ProviderConfigs: []*auth.ProviderConfig{
+			{
+				State: auth.Enabled,
+				TOTPProviderConfig: &auth.TOTPProviderConfig{
+					AdjacentIntervals: 5,
+				},
+			},
+		},
+	}
+	want := &auth.ProjectConfig{
+		MultiFactorConfig: mfaObject,
+	}
+	t.Run("UpdateProjectConfig()", func(t *testing.T) {
+		mfaConfigReq := *want.MultiFactorConfig
+		req := (&auth.ProjectConfigToUpdate{}).
+			MultiFactorConfig(mfaConfigReq)
+		project, err := client.UpdateProjectConfig(context.Background(), req)
+		if err != nil {
+			t.Fatalf("UpdateProjectConfig() = %v", err)
+		}
+		fmt.Printf(cmp.Diff(project, want))
+		if !reflect.DeepEqual(project, want) {
+			t.Errorf("UpdateProjectConfig() = %#v; want = %#v", project, want)
+		}
+	})
+
+	t.Run("GetProjectConfig()", func(t *testing.T) {
+		project, err := client.GetProjectConfig(context.Background())
+		if err != nil {
+			t.Fatalf("GetProjectConfig() = %v", err)
+		}
+
+		if !reflect.DeepEqual(project, want) {
+			t.Errorf("GetProjectConfig() = %v; want = %#v", project, want)
+		}
+	})
+}
