4f9bee4e811deb744ad448d26d55e3e32f38a4d2
Pijush Chakraborty <pijushchakraborty15@gmail.com>
2025-04-05 12:57:19 +0530

    SSRC Client Setup and Fetch Implementation  (#680)
    * Initial Fetch Implementation
    
    * Adding atomic pointer and toJSON method
    
    * Adding ToJson and other config changes
    
    * Updating ServerConfig default and adding basic tests
    
    * Updating headers and adding config options
    
    ---------
    
    Co-authored-by: Pijush Chakraborty <pijushc@google.com>

---
+++ remoteconfig/server_template.go
@@ -0,0 +1,126 @@
+// Copyright 2025 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package remoteconfig
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync/atomic"
+
+	"firebase.google.com/go/v4/internal"
+)
+
+// serverTemplateData stores the internal representation of the server template.
+type serverTemplateData struct {
+	Parameters map[string]parameter `json:"parameters"`
+
+	Version struct {
+		VersionNumber string `json:"versionNumber"`
+		IsLegacy      bool   `json:"isLegacy"`
+	} `json:"version"`
+
+	ETag string
+}
+
+// parameter stores the representation of a template parameter.
+type parameter struct {
+	DefaultValue struct {
+		Value string `json:"value"`
+	} `json:"defaultValue"`
+}
+
+// ServerTemplate represents a template with configuration data, cache, and service information.
+type ServerTemplate struct {
+	rcClient                 *rcClient
+	cache                    atomic.Pointer[serverTemplateData]
+	stringifiedDefaultConfig map[string]string
+}
+
+// NewServerTemplate initializes a new ServerTemplate with optional default configuration.
+func newServerTemplate(rcClient *rcClient, defaultConfig map[string]any) (*ServerTemplate, error) {
+	stringifiedConfig := make(map[string]string, len(defaultConfig)) // Pre-allocate map
+
+	for key, value := range defaultConfig {
+		if value == nil {
+			stringifiedConfig[key] = ""
+			continue
+		}
+
+		// Marshal the value to JSON bytes
+		jsonBytes, err := json.Marshal(value)
+		if err != nil {
+			return nil, fmt.Errorf("failed to marshal config key '%s': %w", key, err)
+		}
+
+		stringifiedConfig[key] = string(jsonBytes)
+	}
+
+	return &ServerTemplate{
+		rcClient:                 rcClient,
+		stringifiedDefaultConfig: stringifiedConfig,
+	}, nil
+}
+
+// Load fetches the server template data from the remote config service and caches it.
+func (s *ServerTemplate) Load(ctx context.Context) error {
+	request := &internal.Request{
+		Method: http.MethodGet,
+		URL:    fmt.Sprintf("%s/v1/projects/%s/namespaces/firebase-server/serverRemoteConfig", s.rcClient.rcBaseURL, s.rcClient.project),
+	}
+
+	templateData := new(serverTemplateData)
+	response, err := s.rcClient.httpClient.DoAndUnmarshal(ctx, request, &templateData)
+
+	if err != nil {
+		return err
+	}
+
+	templateData.ETag = response.Header.Get("etag")
+	s.cache.Store(templateData)
+	return nil
+}
+
+// Set initializes a template using a server template JSON.
+func (s *ServerTemplate) Set(templateDataJSON string) error {
+	templateData := new(serverTemplateData)
+	if err := json.Unmarshal([]byte(templateDataJSON), &templateData); err != nil {
+		return fmt.Errorf("error while parsing server template: %v", err)
+	}
+	s.cache.Store(templateData)
+	return nil
+}
+
+// ToJSON Returns a json representing the cached serverTemplateData.
+func (s *ServerTemplate) ToJSON() (string, error) {
+	jsonServerTemplate, err := json.Marshal(s.cache.Load())
+
+	if err != nil {
+		return "", fmt.Errorf("error while parsing server template: %v", err)
+	}
+
+	return string(jsonServerTemplate), nil
+}
+
+// Evaluate and processes the cached template data.
+func (s *ServerTemplate) Evaluate() *ServerConfig {
+	configMap := make(map[string]value)
+	for key, value := range s.cache.Load().Parameters {
+		configMap[key] = *newValue(Remote, value.DefaultValue.Value)
+	}
+
+	return &ServerConfig{ConfigValues: configMap}
+}
