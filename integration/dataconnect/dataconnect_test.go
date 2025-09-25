756ff9a7a875cbcd1fdd3cd17082bbef17931667
jonathanedey <jedey@google.com>
2025-09-25 11:19:11 -0400

    chore: Fix copyright headers

--- integration/dataconnect/dataconnect_test.go
+++ integration/dataconnect/dataconnect_test.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package dataconnect_test contains integration tests for the dataconnect package.
 package dataconnect
 
 import (
