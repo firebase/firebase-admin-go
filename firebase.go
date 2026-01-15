1fe3c70adaca36917725742a55b6d94905b071d6
Jonathan Edey <145066863+jonathanedey@users.noreply.github.com>
2026-01-15 11:37:50 -0500

    feat(firestore): Added Firestore Multi Database Support (#733)
    * feat(firestore): Add Firestore Multi Database Support
    
    * fix: Address gemini review
    
    * fix: gemini review
    
    * fix: Update name from API review
    
    * fix: Address review comments
    
    * chore: Fix CONTRIBUTING.md typo
    
    Co-authored-by: Lahiru Maramba <llahiru@gmail.com>
    
    ---------
    
    Co-authored-by: Lahiru Maramba <llahiru@gmail.com>

--- firebase.go
+++ firebase.go
@@ -105,10 +105,16 @@ func (a *App) Storage(ctx context.Context) (*storage.Client, error) {
 // Firestore returns a new firestore.Client instance from the https://godoc.org/cloud.google.com/go/firestore
 // package.
 func (a *App) Firestore(ctx context.Context) (*firestore.Client, error) {
+	return a.FirestoreWithDatabaseID(ctx, firestore.DefaultDatabaseID)
+}
+
+// FirestoreWithDatabaseID returns a new firestore.Client instance with the specified named database from the
+// https://godoc.org/cloud.google.com/go/firestore package.
+func (a *App) FirestoreWithDatabaseID(ctx context.Context, databaseID string) (*firestore.Client, error) {
 	if a.projectID == "" {
 		return nil, errors.New("project id is required to access Firestore")
 	}
-	return firestore.NewClient(ctx, a.projectID, a.opts...)
+	return firestore.NewClientWithDatabase(ctx, a.projectID, databaseID, a.opts...)
 }
 
 // InstanceID returns an instance of iid.Client.
